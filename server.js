3a3bee3fdc846c7c3a794bbc451f3ab0f49e7e55
sakethkammari-coder <sakethkammari9@gmail.com>
2026-03-10 22:29:00 +0530

    frontend connected

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
-const Course = require("./models/Course"); // import course model
+const Course = require("./models/Course");
 
 const app = express();
 
@@ -11,17 +11,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 // Test route
 app.get("/", (req, res) => {
   res.send("Backend server is running");
 });
+
 
 // Test API
 app.get("/api/message", (req, res) => {
   res.json({ message: "Hello from backend" });
 });
 
-// Get courses from MongoDB
+
+// 👇 ADD THIS ROUTE HERE
 app.get("/api/courses", async (req, res) => {
   try {
 
@@ -31,17 +34,21 @@ app.get("/api/courses", async (req, res) => {
 
   } catch (error) {
 
+    console.log("MongoDB Error:", error); // prints real error in terminal
     res.status(500).json({ message: "Error fetching courses" });
 
   }
 });
+
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log("MongoDB Connected"))
 .catch((err) => console.log(err));
 
+
 const PORT = process.env.PORT || 5000;
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
