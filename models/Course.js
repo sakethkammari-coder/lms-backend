24f3d39235364dc5172f8f92f3f954d7a2e7fc21
sakethkammari-coder <sakethkammari9@gmail.com>
2026-03-13 12:01:26 +0530

    course_video

--- models/Course.js
+++ models/Course.js
@@ -2,40 +2,45 @@ const mongoose = require("mongoose");
 
 const CourseSchema = new mongoose.Schema({
 
-  id: {
-    type: String,
-    required: true
-  },
+title: {
+type: String,
+required: true
+},
 
-  title: {
-    type: String,
-    required: true
-  },
+instructor: {
+type: String,
+required: true
+},
 
-  instructor: {
-    type: String,
-    required: true
-  },
+duration: {
+type: String
+},
 
-  duration: {
-    type: String
-  },
+level: {
+type: String
+},
 
-  level: {
-    type: String
-  },
+thumbnail: {
+type: String
+},
 
-  thumbnail: {
-    type: String
-  },
+description: {
+type: String
+},
 
-  description: {
-    type: String
-  },
+content: {
+type: String
+},
 
-  content: {
-    type: String
-  }
+videoUrl: {
+type: String
+},
+
+lessons: [
+{
+type: String
+}
+]
 
 });
 
